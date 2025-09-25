2d2de69f9cdd40bf6d40e593d0c5cc4aa3d04748
hamza-sabat1 <hsabat8@gmail.com>
2025-09-25 20:50:09 +0000

    add header props in line with documentation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,26 @@
-function Header() {
+type HeaderProps = {
+  logoSrc: string;                 
+  logoAlt: string;                
+  logoUrl: string;               
+  logoSrText: string;             
+
+  linkLabel1: string;
+  linkUrl1: string;
+  linkLabel2: string;
+  linkUrl2: string;
+};
+
+
+function Header({
+  logoSrc,
+  logoAlt,
+  logoUrl,
+  logoSrText,
+  linkLabel1,
+  linkUrl1,
+  linkLabel2,
+  linkUrl2,
+}: HeaderProps) {
   return (
     <header className="header">
       <nav>
